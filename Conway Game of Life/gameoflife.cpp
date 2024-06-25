b785c81583c2b8a20d03d7b91921b4f7e3e478d8
brysonvb <lbrysonl123@gmail.com>
2024-06-25 08:55:22 -0700

    Added saving and restoring of previous states to Conway Game of Life simulation

--- Conway Game of Life/gameoflife.cpp
+++ Conway Game of Life/gameoflife.cpp
@@ -2,55 +2,69 @@
 #include <string>
 #include <ctime>
 #include <conio.h> // getch
+#include <fstream>
 using namespace std;
 
 const int GRIDSIZE = 15;
 
 void generateNeighborArray(int(&neighbors)[GRIDSIZE][GRIDSIZE], char grid[GRIDSIZE][GRIDSIZE]);
+void displayGrid(const char grid[GRIDSIZE][GRIDSIZE]);
+void initializeGrid(char(&grid)[GRIDSIZE][GRIDSIZE]);
+void saveFile(const char grid[GRIDSIZE][GRIDSIZE]);
+void readFile(char(&grid)[GRIDSIZE][GRIDSIZE]);
 
 int main()
 {
 	char grid[GRIDSIZE][GRIDSIZE];
-	int randomNum;
-	char userInput;
+	char userInput = ' ';
 	int gen = 1;
 
-	srand(time(0));
+	ifstream textFile;
+	textFile.open("saveFile.txt");
 
 	cout << "Welcome to Conway's Game of Life!" << endl << endl;
-	cout << "Initial state:" << endl;
-	for (int i = 0; i < GRIDSIZE; i++)
+	if (textFile)
 	{
-		for (int j = 0; j < GRIDSIZE; j++)
-		{
-			grid[i][j] = (rand() % 2 == 0) ? ' ' : '*';
-			cout << grid[i][j] << "\t";
-		}
+		cout << "Would you like to start from your most recent save state? (y/n) ";
+		while (userInput != 'Y' && userInput != 'y' && userInput != 'N' && userInput != 'n')
+			userInput = _getch();
+
+		if (userInput == 'Y' || userInput == 'y')
+			readFile(grid);
+		else
+			initializeGrid(grid);
 		cout << endl;
 	}
+	else
+	{
+		cout << "You currently have no previous save state. Start the simulation to create one!" << endl << endl;
+		initializeGrid(grid);
+	}
+
+	cout << "Initial state:" << endl;
+	displayGrid(grid);
+			
 	cout << "Press spacebar to start the simulation...";
 	userInput = _getch();
 	while (userInput != ' ')
 		userInput = _getch();
 	cout << endl << endl;
 
+
 	int neighbors[GRIDSIZE][GRIDSIZE];
 
 	while (userInput != 'q' && userInput != 'Q')
 	{
 		cout << "Generation " << gen << ":" << endl;
-		for (int i = 0; i < GRIDSIZE; i++)
-		{
-			for (int j = 0; j < GRIDSIZE; j++)
-				cout << grid[i][j] << "\t";
-			cout << endl;
-		}
+		displayGrid(grid);
 		cout << endl;
-		cout << "Press spacebar to continue to Generation 2, or \'q\' to quit... ";
+
+		cout << "Press spacebar to continue to Generation " << gen + 1 << ", or \'q\' to quit... ";
 		userInput = _getch();
 		while (userInput != ' ' && userInput != 'q')
 			userInput = _getch();
 		cout << endl << endl;
+
 		if (userInput == ' ')
 		{
 			generateNeighborArray(neighbors, grid);
@@ -73,6 +87,8 @@ int main()
 		}
 		gen++;
 	}
+
+	saveFile(grid);
 }
 
 void generateNeighborArray(int(&neighbors)[GRIDSIZE][GRIDSIZE], char grid[GRIDSIZE][GRIDSIZE])
@@ -93,8 +109,6 @@ void generateNeighborArray(int(&neighbors)[GRIDSIZE][GRIDSIZE], char grid[GRIDSI
 				if (grid[i + 1][j + k] == '*')
 					neighbors[i][j] += 1;
 			}
-			if (grid[i][j] == '*')
-				neighbors[i][j] -= 1; // neighbors [i][j] was included as a neighbor
 		}
 	}
 	for (int i = 1; i < GRIDSIZE - 1; i++)
@@ -118,13 +132,99 @@ void generateNeighborArray(int(&neighbors)[GRIDSIZE][GRIDSIZE], char grid[GRIDSI
 			if (grid[i + k][GRIDSIZE - 2] == '*')
 				neighbors[i][GRIDSIZE - 1] += 1;
 		}
-		if (neighbors[0][i] == '*')
-			neighbors[0][i] -= 1; // neighbors [0][i] was included as a neighbor
-		if (neighbors[GRIDSIZE - 1][i] == '*')
-			neighbors[GRIDSIZE - 1][i] -= 1; // neighbors [GRIDSIZE - 1][i] was included as a neighbor
-		if (neighbors[i][0] == '*')
-			neighbors[i][0] -= 1; // neighbors [i][0] was included as a neighbor
-		if (neighbors[i][GRIDSIZE - 1] == '*')
-			neighbors[i][GRIDSIZE - 1] -= 1; // neighbors [i][GRIDSIZE - 1] was included as a neighbor
+	}
+
+	// top left corner
+	if (grid[0][1] == '*')
+		neighbors[0][0] += 1;
+	if (grid[1][1] == '*')
+		neighbors[0][0] += 1;
+	if (grid[1][0] == '*')
+		neighbors[0][0] += 1;
+	neighbors[0][0] += 1;
+
+	// top right corner
+	if (grid[0][GRIDSIZE - 2] == '*')
+		neighbors[0][GRIDSIZE - 1] += 1;
+	if (grid[1][GRIDSIZE - 1] == '*')
+		neighbors[0][GRIDSIZE - 1] += 1;
+	if (grid[1][GRIDSIZE - 2] == '*')
+		neighbors[0][GRIDSIZE - 1] += 1;
+	neighbors[0][GRIDSIZE - 1] += 1;
+
+	// bottom left corner
+	if (grid[GRIDSIZE - 2][0] == '*')
+		neighbors[GRIDSIZE - 1][0] += 1;
+	if (grid[GRIDSIZE - 2][1] == '*')
+		neighbors[GRIDSIZE - 1][0] += 1;
+	if (grid[GRIDSIZE - 1][1] == '*')
+		neighbors[GRIDSIZE - 1][0] += 1;
+	neighbors[GRIDSIZE - 1][0] += 1;
+
+	// bottom right corner
+	if (grid[GRIDSIZE - 1][GRIDSIZE - 2] == '*')
+		neighbors[GRIDSIZE - 1][GRIDSIZE - 1] += 1;
+	if (grid[GRIDSIZE - 2][GRIDSIZE - 2] == '*')
+		neighbors[GRIDSIZE - 1][GRIDSIZE - 1] += 1;
+	if (grid[GRIDSIZE - 2][GRIDSIZE - 1] == '*')
+		neighbors[GRIDSIZE - 1][GRIDSIZE - 1] += 1;
+	neighbors[GRIDSIZE - 1][GRIDSIZE - 1] += 1;
+
+	for (int i = 0; i < GRIDSIZE; i++)
+		for (int j = 0; j < GRIDSIZE; j++)
+			if (grid[i][j] == '*')
+				neighbors[i][j] -= 1;
+}
+
+void displayGrid(const char grid[GRIDSIZE][GRIDSIZE])
+{
+	for (int i = 0; i < GRIDSIZE; i++)
+	{
+		for (int j = 0; j < GRIDSIZE; j++)
+			cout << grid[i][j] << "\t";
+		cout << endl;
+	}
+}
+
+void initializeGrid(char(&grid)[GRIDSIZE][GRIDSIZE])
+{
+	srand(time(0));
+
+	for (int i = 0; i < GRIDSIZE; i++)
+	{
+		for (int j = 0; j < GRIDSIZE; j++)
+			grid[i][j] = (rand() % 2 == 0) ? ' ' : '*';
+	}
+}
+
+void saveFile(const char grid[GRIDSIZE][GRIDSIZE])
+{
+	ofstream output("saveFile.txt");
+
+	for (int i = 0; i < GRIDSIZE; i++)
+	{
+		for (int j = 0; j < GRIDSIZE; j++)
+		{
+			if (grid[i][j] == ' ')
+				output << "s" << " ";
+			else
+				output << grid[i][j] << " ";
+		}
+		output << endl;
+	}
+}
+
+void readFile(char(&grid)[GRIDSIZE][GRIDSIZE])
+{
+	ifstream input("saveFile.txt");
+
+	for (int i = 0; i < GRIDSIZE; i++)
+	{
+		for (int j = 0; j < GRIDSIZE; j++)
+		{
+			input >> grid[i][j];
+			if (grid[i][j] == 's')
+				grid[i][j] = ' ';
+		}
 	}
 }
