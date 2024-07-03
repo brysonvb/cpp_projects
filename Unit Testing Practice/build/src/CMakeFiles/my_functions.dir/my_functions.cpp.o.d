868def91c7767bee57a42840ba29d51d0a48dc0e
brysonvb <lbrysonl123@gmail.com>
2024-07-03 09:07:37 -0700

    Removed build folder from Unit Testing Practice folder

--- Unit Testing Practice/build/src/CMakeFiles/my_functions.dir/my_functions.cpp.o.d
+++
@@ -1,3 +0,0 @@
-src/CMakeFiles/my_functions.dir/my_functions.cpp.o: \
- /home/bryson/MyProject/src/my_functions.cpp /usr/include/stdc-predef.h \
- /home/bryson/MyProject/include/my_functions.h
--- Unit Testing Practice/build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bryson/MyProject/tests
-# Build directory: /home/bryson/MyProject/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_my_functions "/home/bryson/MyProject/build/tests/test_my_functions")
-set_tests_properties(test_my_functions PROPERTIES  _BACKTRACE_TRIPLES "/home/bryson/MyProject/tests/CMakeLists.txt;10;add_test;/home/bryson/MyProject/tests/CMakeLists.txt;0;")
